b0897c8ff97f57a195d12d8e004316285b075e9b
Lissie Chin <lissiechin@gmail.com>
2021-01-03 21:04:11 -0800

    maui gallery in process

--- Photogallery.js
+++ Photogallery.js
@@ -1,3 +1,24 @@
+/* scroll to top */
+//Get the button
+var mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+scrollFunction()
+};
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+/*            end of scroll to top      */
+
 /*-----------------------MODAL JS -------------------*/
 function openModal() {
   document.getElementById("myModal").style.display = "block";
@@ -35,28 +56,10 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
 /*----------------------------------------------*/
 
-/* scroll to top */
-//Get the button
-var mybutton = document.getElementById("myBtn");
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-scrollFunction()
-};
-function scrollFunction() {
-if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-mybutton.style.display = "block";
-} else {
-mybutton.style.display = "none";
-}
-}
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
-}
-/*            end of scroll to top      */
+
 /* MENU NAV button */
 function openNav() {
 document.getElementById("mySidenav").style.width = "250px";
