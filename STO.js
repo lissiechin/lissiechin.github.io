4dfe99eaca0d464f06bcf4bad3f608ddf934d761
Lissie Chin <lissiechin@gmail.com>
2021-06-21 20:32:40 -0700

    adding taiwan photos and STO pages updating

--- STO.js
+++ STO.js
@@ -1,4 +1,5 @@
 /* STO_JS */
+/*navigation menu */
  function openNav() {
                         document.getElementById("mySidenav").style.width = "250px";
                         document.getElementById("main").style.marginLeft = "250px";
@@ -25,3 +26,18 @@
                             }
                         });
                     }
+/* accordion js */
+    var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
