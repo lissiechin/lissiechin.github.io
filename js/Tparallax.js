a2dbf5624a0a7fe53b0daaec7457a3780f6388aa
lissiechin <lissiechin@gmail.com>
2021-08-30 16:57:03 -0700

    parallax photo page

--- js/Tparallax.js
+++ js/Tparallax.js
@@ -1,58 +1,5 @@
 
-    /* -------Parallax Slider ----------- */
-
-    let images = [...document.querySelectorAll('.img')];
-    let slider = document.querySelector('.slider');
-    let sliderwidth;
-    let imagewidth;
-    let current = 0;
-    let target = 0;
-    let idx = 0;
-    let ease = .05; /* speed of the scrolling */
-
-    window, addEventListener('resize',init);
-
-    images.forEach((img, indx) => {
-        img.style.backgroundImage = `url(/Content/images/TravelSlider/${idx+1}.jpg)`
-    })
-
-    function lerp(start, end, t){
-        return start * (1-t) + end * t;
-    }
-
-    function setTransform(el, transform){
-        el.style.transform = transform;
-    }
-
-    function init(){
-        sliderwidth = slider.getBoundClientRect().width;
-        imagewidth = sliderwidth / image.length;
-        document.body.style.height = `${sliderwidth - (window.innerWidth - window.innerHeight)}px`
-
-    }
-
-    function animate(){
-        current = parseFloat(lerp(current, target, ease)).toFixed(2);
-        target = window.scrollY;
-        setTransform(slider, `translateX(-${curent}px`)
-        requestAnimationFrame(animate)
-    }
-
-    function animateImages(){
-        let ratio = current / imagewidth;
-        let intersectionRatioValue;
-
-        images.forEach((image, idx) => {
-            intersectionRatioValue = ratio - (idx * 0.7);
-            setTransform(image,`translateX(${intersectionRatioValue * 70}px`)
-        })
-    }
-
-    init();
-    animate();
-
-
-
+   
 /* ---------- MENU NAV button ------------- */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -80,6 +27,13 @@ for (i = 0; i < dropdown.length; i++) {
         }
     });
 
+ /* -------Parallax Slider ----------- */
+
+ jQuery(document).ready(function($){
+  'use strict';
+
+  $.Scrollax();
+});
 
 
 
