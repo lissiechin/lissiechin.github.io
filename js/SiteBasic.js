e7729b4eebb46e852b2706e6aab28f12996efb40
lissiechin <lissiechin@gmail.com>
2021-08-30 15:50:58 -0700

    tab icon and other edits with the parallax

--- js/SiteBasic.js
+++ js/SiteBasic.js
@@ -1,61 +1,24 @@
-/* scroll to top */
-//Get the button
-var mybutton = document.getElementById("myBtn");
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-scrollFunction()
-};
-function scrollFunction() {
-if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-mybutton.style.display = "block";
-} else {
-mybutton.style.display = "none";
-}
-}
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-document.body.scrollTop = 0;
-document.documentElement.scrollTop = 0;
-}
-/*            end of scroll to top      */
+// /* scroll to top */
+// //Get the button
+// var mybutton = document.getElementById("myBtn");
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {
+// scrollFunction()
+// };
+// function scrollFunction() {
+// if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+// mybutton.style.display = "block";
+// } else {
+// mybutton.style.display = "none";
+// }
+// }
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+// document.body.scrollTop = 0;
+// document.documentElement.scrollTop = 0;
+// }
+// /*            end of scroll to top      */
 
-/*-----------------------MODAL JS -------------------*/
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
 
 /*----------------------------------------------*/
 
@@ -84,4 +47,10 @@ dropdownContent.style.display = "none";
 } else {
 dropdownContent.style.display = "block";
 }
+});
+/* parallax */
+jQuery(document).ready(function($){
+  'use strict';
+
+  $.Scrollax();
 });
