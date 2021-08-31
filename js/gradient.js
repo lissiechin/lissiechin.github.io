bff66aa47ec03f97187004d3261002cff59c1152
lissiechin <lissiechin@gmail.com>
2021-08-30 22:03:15 -0700

    gradient tool

---
+++ js/gradient.js
@@ -0,0 +1,16 @@
+var css = document.querySelector("h4")
+var color1 = document.querySelector("color1") 
+var color2 = document.querySelector("color2")
+var gradient = document.querySelector("gradient")
+
+function setGradient(){
+	gradient.style.backgroud = "linear-gradient (45deg, "
+	+ cplor1.value
+	+", "
+	+color2.value
+	+")";
+	css.textContent = gradient.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
