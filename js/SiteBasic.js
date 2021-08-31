44f0ea355cfca3bc5ea83c3f44145380f289c936
lissiechin <lissiechin@gmail.com>
2021-08-30 20:22:51 -0700

    js and css

--- js/SiteBasic.js
+++ js/SiteBasic.js
@@ -1,4 +1,3 @@
-
 /* MENU NAV button */
 function openNav() {
 document.getElementById("mySidenav").style.width = "250px";
@@ -26,9 +25,8 @@ dropdownContent.style.display = "block";
 });
 /* parallax */
 jQuery(document).ready(function($){
-  'use strict';
-
-  $.Scrollax();
+'use strict';
+$.Scrollax();
 });
 /* -------------------------------------------*/
 /* scroll to top */
@@ -51,60 +49,8 @@ document.body.scrollTop = 0;
 document.documentElement.scrollTop = 0;
 }
 /*            end of scroll to top      */
-
-
 /*----------------------------------------------*/
-/* ----------footer styling ----------------*/
-#content-wrap {
-padding-bottom: 2.5px;
-/* Footer height  & em refers to the font-size (2.5 times the font size */
-}
-/*    begining new layout CSS     */
-}
-.column {
-float: left;
-width: 25%;
-/* 33.33 for 3 images*/
-padding: 5px;
-}
-/* Clearfix (clear floats) */
-.row::after {
-content: "";
-clear: both;
-display: table;
-}
-/* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
-@media screen and (max-width: 500px) {
-.column {
-width: 100%;
-}
-}
-.container {
-position: relative;
-max-width: 800px;
-/* Maximum width */
-margin: 0 auto;
-/* Center it */
-}
-.container .content {
-position: absolute;
-/* Position the background text */
-bottom: 0;
-/* At the bottom. Use top:0 to append it to the top */
-background: rgb(0, 0, 0);
-/* Fallback color */
-background: rgba(0, 0, 0, 0.5);
-/* Black background with 0.5 opacity */
-color: #f1f1f1;
-/* Grey text */
-width: 100%;
-/* Full width */
-padding: 20px;
-/* Some padding */
-height: auto;
--webkit-animation: fadeEffect 1s;
-animation: fadeEffect 1s;
-}
+
 /* Fade in tabs */
 @-webkit-keyframes fadeEffect {
 from {
