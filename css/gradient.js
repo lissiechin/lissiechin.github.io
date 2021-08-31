3d549444db90fb1c08d52cf219db2bd7733e8cc7
lissiechin <lissiechin@gmail.com>
2021-08-30 22:38:20 -0700

    editing

--- css/gradient.js
+++ css/gradient.js
@@ -1,14 +1,12 @@
 var css = document.querySelector("h4")
 var color1 = document.querySelector(".color1") 
 var color2 = document.querySelector(".color2")
-var gradient = document.querySelector("gradient")
+var gradient = document.querySelector(".gradient")
 
 function setGradient(){
 	gradient.style.backgroud = "linear-gradient (45deg, "
-	+ color1.value
-	+", "
-	+color2.value
-	+")";
+	+ color1.value +", "
+	+color2.value +")";
 	css.textContent = gradient.style.background + ";";
 }
 
