5e8754ec0b1b9c62eff1c14eac43690bd2a76261
lissiechin <lissiechin@gmail.com>
2021-08-30 20:16:37 -0700

    tweeking

--- js/SiteBasic.js
+++ js/SiteBasic.js
@@ -1,27 +1,3 @@
-// /* scroll to top */
-// //Get the button
-// var mybutton = document.getElementById("myBtn");
-// // When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function() {
-// scrollFunction()
-// };
-// function scrollFunction() {
-// if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-// mybutton.style.display = "block";
-// } else {
-// mybutton.style.display = "none";
-// }
-// }
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-// document.body.scrollTop = 0;
-// document.documentElement.scrollTop = 0;
-// }
-// /*            end of scroll to top      */
-
-
-/*----------------------------------------------*/
-
 
 /* MENU NAV button */
 function openNav() {
@@ -54,7 +30,30 @@ jQuery(document).ready(function($){
 
   $.Scrollax();
 });
+/* -------------------------------------------*/
+/* scroll to top */
+//Get the button
+var mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+scrollFunction()
+};
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+document.body.scrollTop = 0;
+document.documentElement.scrollTop = 0;
+}
+/*            end of scroll to top      */
 
+
+/*----------------------------------------------*/
 /* ----------footer styling ----------------*/
 #content-wrap {
 padding-bottom: 2.5px;
